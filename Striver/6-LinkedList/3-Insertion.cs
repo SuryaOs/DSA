766711d237f3e4e4a79c37c4fad604212bc60f32
Surya <ossurya97@gmail.com>
2025-08-10 09:26:15 +0530

    LL Insertion

---
+++ Striver/6-LinkedList/3-Insertion.cs
@@ -0,0 +1,79 @@
+namespace dsaproblem.Striver.LinkedList;
+
+public class Insertion
+{
+    public static void Brute()
+    {
+        int[] arr = { 1, 2, 3, 4 };
+        Node head = Intro.ArrayToLL(arr);
+        // Node newHead = InsertHead(null, 100);
+        // Node newHead = InsertTail(null, 100);
+        // Node newHead = InsertK(head, 3, 100);
+        Node newHead = InsertBefore(head, 10, 5);
+        Intro.Print(newHead);
+    }
+
+    public static Node InsertHead(Node head, int value)
+    {
+        return new Node(value, head);
+    }
+
+    public static Node InsertTail(Node head, int value)
+    {
+        if (head == null)
+            return new Node(value);
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = new Node(value);
+        return head;
+    }
+
+    public static Node InsertK(Node head, int k, int value)
+    {
+        if (head == null)
+        {
+            if (k == 1)
+                return new Node(value);
+            else
+                return null;
+        }
+        if (k == 1)
+            return InsertHead(head, value);
+
+        Node temp = head;
+        int count = 0;
+        while (temp != null)
+        {
+            if (++count == k - 1)
+            {
+                Node newNode = new Node(value, temp.next);
+                temp.next = newNode;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+    public static Node InsertBefore(Node head, int val, int k)
+    {
+        if (head == null) return null;
+        if (head.data == k) return new Node(val, head);
+        Node temp = head;
+        Node prev = null;
+        while (temp != null)
+        {
+            if (temp.data == k)
+            {
+                var n = new Node(val);
+                n.next = temp;
+                prev.next = n;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+}
--- Striver/6-LinkedList/LinkedListExtension.cs
+++ Striver/6-LinkedList/LinkedListExtension.cs
@@ -5,6 +5,7 @@ public class LinkedListExtension
     public static void Practice()
     {
         // Intro.Brute(); // ConvertArrayToLL, TraverseAndPrint, LengthOfLL
-        Deletion.Brute(); // Delete Head, Tail, Kth Node, K elem
+        // Deletion.Brute(); // Delete Head, Tail, Kth Node, K elem
+        Insertion.Brute(); // Insert Head, Tail, KthNode, Before K Elem
     }
 }
