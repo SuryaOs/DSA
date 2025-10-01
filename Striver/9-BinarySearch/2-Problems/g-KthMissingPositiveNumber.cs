34278ac588b18b95ef7f31d7bf4048c67a38a86d
Surya <ossurya97@gmail.com>
2025-10-01 20:50:18 +0530

    mssing positive number optimal

--- Striver/9-BinarySearch/2-Problems/g-KthMissingPositiveNumber.cs
+++ Striver/9-BinarySearch/2-Problems/g-KthMissingPositiveNumber.cs
@@ -4,9 +4,10 @@ public class KthMissingPositiveNumber
 {
     public static void Run()
     {
-        int[] a = { 1, 2, 3, 4 };
-        int k = 2;
-        Console.WriteLine(Naivest(a, k));
+        int[] a = { 3, 4, 5 };
+        int k = 3;
+        Console.WriteLine(Naive(a, k));
+        Console.WriteLine(Optimal(a, k));
     }
     private static int Naivest(int[] a, int k)
     {
@@ -26,6 +27,45 @@ public class KthMissingPositiveNumber
             }
         }
         return -1;
+    }
+    public static int Naive(int[] a, int k)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] <= k)
+            {
+                k++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return k;
+    }
+    public static int Optimal(int[] a, int k)
+    {
+        int low = 0;
+        int high = a.Length - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int missing = a[mid] - (mid + 1);
+            if (missing < k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        // deriving formula
+        // int noOfMissingElementsBeforeHigh = a[high] - (high + 1);  => a[high] - high - 1;
+        // int ans = a[high] + (k - noOfMissingElementsBeforeHigh); 
+        // int ans = a[high] + k - a[high] + high + 1;
+        int ans = k + low;
+        return ans;
     }
     private static bool Missing(int[] a, int num)
     {
